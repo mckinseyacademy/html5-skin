63bd8ea7b047cc9dd310350a842bac95c5ea3cd4
Naeem Ilyas <naeem-ilyas@live.com>
2016-11-03 13:21:33 +0500

    UI customizations to match mcka v3 player UI

--- js/components/closed-caption/closedCaptionPopover.js
+++ js/components/closed-caption/closedCaptionPopover.js
@@ -2,6 +2,7 @@ var React = require('react'),
     CONSTANTS = require('../../constants/constants'),
     Utils = require('../utils'),
     OnOffSwitch = require('./onOffSwitch'),
+    ClassNames = require('classnames'),
     CloseButton = require('../closeButton');
 
 var ClosedCaptionPopover = React.createClass({
@@ -15,20 +16,49 @@ var ClosedCaptionPopover = React.createClass({
     this.props.togglePopoverAction();
   },
 
+  handleChangeLanguage: function (language) {
+    var availableLanguages = this.props.closedCaptionOptions.availableLanguages;
+    var invertedLocale = {};
+    for (var i = 0; i < availableLanguages.languages.length; i++) {
+      invertedLocale[availableLanguages.locale[availableLanguages.languages[i]]] = availableLanguages.languages[i];
+    }
+
+    if (!this.props.closedCaptionOptions.enabled) {
+      this.props.controller.toggleClosedCaptionEnabled();
+    }
+
+    this.props.controller.onClosedCaptionChange('language', invertedLocale[language]);
+    this.setState({
+      selectedLanguage: language
+    });
+  },
+
+  setClassname: function(item) {
+    var selectedLanguage = this.props.closedCaptionOptions.availableLanguages.locale[this.props.closedCaptionOptions.language]
+    return ClassNames({
+      'oo-item': true,
+      'oo-item-selected': selectedLanguage == item && this.props.closedCaptionOptions.enabled,
+      'oo-disabled': !this.props.closedCaptionOptions.enabled
+    });
+  },
+
   render: function() {
     var captionBtnText = Utils.getLocalizedString(this.props.language, CONSTANTS.SKIN_TEXT.CC_OPTIONS, this.props.localizableStrings);
 
+    var availableLanguages = this.props.closedCaptionOptions.availableLanguages.locale;
+    var dataContentBlocks = [];
+    for (var i in availableLanguages) {
+      dataContentBlocks.push(
+       <a className={this.setClassname(availableLanguages[i])} onClick={this.handleChangeLanguage.bind(this, availableLanguages[i])} key={i}>{availableLanguages[i]}</a>
+      );
+    }
+
     return (
       <ul className="oo-popover-horizontal">
-        <li>
-          <OnOffSwitch {...this.props} />
-        </li>
-        <li>
-          <a className="oo-more-captions" onClick={this.handleMoreCaptions}>{captionBtnText}</a>
-        </li>
-        <li>
-          <CloseButton {...this.props} closeAction={this.handleClose} />
-        </li>
+        <div className="custom-cc-lang-block">
+            {dataContentBlocks}
+        </div>
+        <OnOffSwitch {...this.props} />
       </ul>
     );
   }
